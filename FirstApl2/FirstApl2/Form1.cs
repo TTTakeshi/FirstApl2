cf30d1b03b32e72a20a905d8a089362ca0e00d74
takeshi <takiguchi.takeshi123@gmail.com>
2017-10-15 14:02:58 +0900

    Socekt通信 基本OK

--- FirstApl2/FirstApl2/Form1.cs
+++ FirstApl2/FirstApl2/Form1.cs
@@ -20,8 +20,74 @@ namespace FirstApl2
         private void button1_Click(object sender, EventArgs e)
         {
 
-            // ボタン追加
 
+            // サーバーのIPアドレス（または、ホスト名）とポート番号
+            string ipOrHost = "127.0.0.1";
+            int port = 1024;
+
+            System.Net.Sockets.TcpClient tcp = new System.Net.Sockets.TcpClient(ipOrHost, port);
+
+            // NetworkStriamを取得する
+            System.Net.Sockets.NetworkStream ns = tcp.GetStream();
+
+            //
+            ns.ReadTimeout = 10000;
+            ns.WriteTimeout = 10000;
+
+
+            string sendMsg = this.textBox1.Text;
+
+            // サーバーにデータを送信する
+            // 文字列をbyte型配列に変換
+            System.Text.Encoding enc = System.Text.Encoding.UTF8;
+
+            //            byte[] sendBytes = enc.GetBytes(sendMsg + '\n');
+            //            byte[] sendBytes = { 0x01, 0x02, 0x03, 0x04 };
+
+            byte[] sendBytes = new byte[sendMsg.Length];
+
+            for (int i = 0; i < sendMsg.Length; i++)
+            {
+                sendBytes[i] = (byte)sendMsg[i];
+            }
+
+            sendBytes[0] = 0x00;
+            sendBytes[1] = 0x01;
+
+            byte[] buf = Encoding.ASCII.GetBytes(sendMsg);
+
+            byte[] buf2 = FromHexString(sendMsg); ;
+            //            buf2[0] = BitConverter.ToSingle(buf[0], 0);
+
+            // データを送信する
+            //            ns.Write(sendBytes, 0, sendBytes.Length);
+            ns.Write(buf2, 0, buf2.Length);
+
+            // コミットテスト
+
+            // 閉じる
+            ns.Close();
+            tcp.Close();
+
+
+        }
+
+        /// <summary>
+        /// 16進数の文字列からバイト配列を生成します。
+        /// </summary>
+        /// <param name="str">16進数文字列</param>
+        /// <returns>バイト配列</returns>
+        public static byte[] FromHexString(string str)
+        {
+            int length = str.Length / 2;
+            byte[] bytes = new byte[length];
+            int j = 0;
+            for (int i = 0; i < length; i++)
+            {
+                bytes[i] = Convert.ToByte(str.Substring(j, 2), 16);
+                j += 2;
+            }
+            return bytes;
         }
     }
 }
